0d3003e0a9ee17839f71238e32af33ae300a580e
pangyu <1510397456@qq.com>
2023-01-27 16:28:21 +0800

    feat: 完成两种用户的注册和注销功能(通过注解实现数据格式验证)

--- cloud-user/src/main/java/com/numb_little_bug/controller/UserController.java
+++ cloud-user/src/main/java/com/numb_little_bug/controller/UserController.java
@@ -6,9 +6,11 @@ import com.numb_little_bug.mapper.UserMapper;
 import com.numb_little_bug.mapper.IDCardMapper;
 import com.numb_little_bug.utils.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -30,22 +32,59 @@ public class UserController {
         return userMapper.queryUserById(id);
     }
 
-    //接受前端用户注册表单
-    @PostMapping(value="/user/register")
-    public JsonResult register(@RequestBody User user){
+    // 接受前端用户注册表单
+    @PostMapping(value="/user")
+    public JsonResult register(@Valid User user, BindingResult bindingResult){
+        // 判断数据格式是否有错误
+        if (bindingResult.hasErrors()) {
+            return new JsonResult(401, bindingResult.getFieldError().getDefaultMessage(), null);
+        }
+
+        // 判断用户是否已经注册
+        User existingUser = userMapper.queryUserByTel(user.getTel());
+        if (existingUser != null) {
+            return new JsonResult(402, "该手机号已经被注册", null);
+        }
+
+        user.setCreated_time(new java.util.Date());
         try{
             userMapper.addUser(user);
         } catch (Exception e){
-            //返回json格式包含状态码的错误信息
+            // 返回json格式包含状态码的错误信息
             JsonResult jr = new JsonResult();
-            jr.setCode(500);
-            jr.setMsg("注册失败");
+            jr.setCode(410);
+            jr.setMsg("注册失败, "+e.getMessage());
             return jr;
         }
-        //返回json格式包含状态码的成功信息
+        // 返回json格式包含状态码的成功信息
         JsonResult jr = new JsonResult();
         jr.setCode(0);
         jr.setMsg("注册成功");
         return jr;
     }
+
+    // 注销用户
+    @DeleteMapping(value="/user/{id}")
+    public JsonResult deleteUser(@PathVariable("id") Long id) {
+        // 判断用户是否存在
+        User existingUser = userMapper.queryUserById(id);
+        if (existingUser == null) {
+            return new JsonResult(404, "该用户不存在", null);
+        }
+        try{
+            userMapper.deleteUser(id);
+        } catch (Exception e){
+            // 返回json格式包含状态码的错误信息
+            JsonResult jr = new JsonResult();
+            jr.setCode(410);
+            jr.setMsg("注销失败, "+e.getMessage());
+            return jr;
+        }
+        // 返回json格式包含状态码的成功信息
+        JsonResult jr = new JsonResult();
+        jr.setCode(0);
+        jr.setMsg("注销成功");
+        return jr;
+    }
+
 }
--- cloud-user/src/main/java/com/numb_little_bug/entity/User.java
+++ cloud-user/src/main/java/com/numb_little_bug/entity/User.java
@@ -4,8 +4,15 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.context.annotation.Primary;
 
+import javax.annotation.RegEx;
 import javax.annotation.sql.DataSourceDefinition;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 import java.util.Objects;
 
@@ -14,10 +21,20 @@ import java.util.Objects;
 public class User {
     @TableId(type = IdType.AUTO)
     private Integer id;
+
+    @Length(min = 1, message = "姓名不能为空")
     private String name;
+
+    @Length(min = 6, max = 16, message = "密码长度必须在6-16位之间")
     private String password;
+
+    @Pattern(regexp = "^1[3|4|5|7|8][0-9]\\d{8}$", message = "手机号格式不正确")
     private String tel;
+
     private IDCard id_card;
-    private Date create_time;
+
+    private Date created_time;
+
+    @Pattern(regexp = "^(site|dispatch)$", message = "角色只能是site或者dispatch")
     private String role;
 }
