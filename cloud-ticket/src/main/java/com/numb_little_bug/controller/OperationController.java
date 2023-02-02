1aa8ca096f4d161ab335a6d3580fd6595f301e76
pangyu <1510397456@qq.com>
2023-02-02 17:15:27 +0800

    feat: 完善ticket服务的接口; 完善ticket服务的注释以生成API文档

--- cloud-ticket/src/main/java/com/numb_little_bug/controller/OperationController.java
+++ cloud-ticket/src/main/java/com/numb_little_bug/controller/OperationController.java
@@ -3,8 +3,7 @@ package com.numb_little_bug.controller;
 
 import com.numb_little_bug.entity.Operation;
 import com.numb_little_bug.mapper.OperationMapper;
-import com.numb_little_bug.mapper.TicketMapper;
-import com.numb_little_bug.utils.JsonResult;
+import com.numb_little_bug.mapper.JsonResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,21 +14,38 @@ public class OperationController {
         this.operationMapper = operationMapper;
     }
 
+    /**
+     * 根据操作id查询单步操作
+     * @return JsonResult
+     */
     @GetMapping("operation/{id}")
     public JsonResult queryOperationById(@PathVariable("id") Integer id) {
         return new JsonResult(0, operationMapper.queryOperationById(id), "查询成功", "success");
     }
 
+    /**
+     * 根据操作次序号查询单步操作
+     * @return JsonResult
+     */
     @GetMapping("operation/stepNumber/{stepNumber}")
     public JsonResult queryOperationByStepNumber(@PathVariable("stepNumber") Integer stepNumber) {
         return new JsonResult(0, operationMapper.queryOperationByStepNumber(stepNumber), "查询成功", "success");
     }
 
+    /**
+     * 根据操作票id查询所有操作
+     * @return JsonResult
+     */
     @GetMapping("operation/ticket/{ticketId}")
     public JsonResult queryOperationByTicketId(@PathVariable("ticketId") Integer ticketId) {
         return new JsonResult(0, operationMapper.queryOperationByTicketId(ticketId), "查询成功", "success");
     }
 
+    /**
+     * 添加单步操作
+     * @param operation 单次操作
+     * @return JsonResult
+     */
     @PostMapping("operation")
     public JsonResult addOperation(@RequestBody Operation operation) {
         try {
@@ -40,6 +56,11 @@ public class OperationController {
         return new JsonResult(0, null, "添加成功", "success");
     }
 
+    /**
+     * 更新单步操作
+     * @param operation 单次操作
+     * @return JsonResult
+     */
     @PutMapping("operation")
     public JsonResult updateOperation(@RequestBody Operation operation) {
         try {
@@ -50,6 +71,11 @@ public class OperationController {
         return new JsonResult(0, null, "更新成功", "success");
     }
 
+    /**
+     * 删除单步操作
+     * @param id 单次操作id
+     * @return JsonResult
+     */
     @DeleteMapping("operation/{id}")
     public JsonResult deleteOperation(@PathVariable("id") Integer id) {
         try {
--- cloud-ticket/src/main/java/com/numb_little_bug/mapper/TicketMapper.java
+++ cloud-ticket/src/main/java/com/numb_little_bug/mapper/TicketMapper.java
@@ -9,21 +9,58 @@ public interface TicketMapper {
     @Select("select * from ticket where id = #{id}")
     Ticket queryTicketById(Integer id);
 
+    @Select("select * from ticket where siteId = #{siteId}")
+    Ticket[] queryTicketBySiteId(Integer siteId);
+
     @Select("select * from ticket where name = #{name}")
     Ticket queryTicketByName(String name);
 
     @Select("select * from ticket where teller = #{teller}")
     Ticket[] queryTicketByTeller(String teller);
 
-    @Select("insert into ticket (id, name, teller, notice) values (#{id}, #{name}, #{teller}, #{notice})")
+    @Select("select * from ticket where operator = #{operator}")
+    Ticket[] queryTicketByOperator(String operator);
+
+    @Select("select * from ticket where publisher = #{publisher}")
+    Ticket[] queryTicketByPublisher(String publisher);
+
+    @Select("select * from ticket where status = #{status}")
+    Ticket[] queryTicketByStatus(String status);
+
+    @Select("insert into ticket(name, teller, operator, notice, siteId, startTime, endTime, publisher, remark, status) values(#{name}, #{teller}, #{operator}, #{notice}, #{siteId}, #{startTime}, #{endTime}, #{publisher}, #{remark}, #{status})")
     void addTicket(Ticket ticket);
 
     @Select("delete from ticket where id = #{id}")
     void deleteTicket(Integer id);
 
-    @Select("update ticket set name = #{name}, teller = #{teller}, notice = #{notice} where id = #{id}")
+    @Select("update ticket set name = #{name}, teller = #{teller}, operator = #{operator}, notice = #{notice}, siteId = #{siteId}, startTime = #{startTime}, endTime = #{endTime}, publisher = #{publisher}, remark = #{remark}, status = #{status} where id = #{id}")
     void updateTicket(Ticket ticket);
 
     @Select("select * from ticket")
     Ticket[] queryAllTicket();
+
+    @Select("update ticket set status = #{status} where id = #{id}")
+    void updateTicketStatus(String status, Integer id);
+
+    @Select("update ticket set name = #{name} where id = #{id}")
+    void updateTicketName(String name, Integer id);
+
+    @Select("update ticket set teller = #{teller} where id = #{id}")
+    void updateTicketTeller(String teller, Integer id);
+
+    @Select("update ticket set operator = #{operator} where id = #{id}")
+    void updateTicketOperator(String operator, Integer id);
+
+    @Select("update ticket set notice = #{notice} where id = #{id}")
+    void updateTicketNotice(String notice, Integer id);
+
+    @Select("update ticket set siteId = #{siteId} where id = #{id}")
+    void updateTicketSiteId(Integer siteId, Integer id);
+
+    @Select("update ticket set startTime = #{startTime}, endTime = #{endTime} where id = #{id}")
+    void updateTicketTime(String startTime, String endTime, Integer id);
+
+    @Select("update ticket set remark = #{remark} where id = #{id}")
+    void updateTicketRemark(String remark, Integer id);
+
 }
